edce7a518f1bb831aa39429c62efbdde4f4a7c9d
rafaelruiznavas <rafa@ruiznavas.com>
2022-02-20 18:56:49 +0100

    Add avatar

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,21 +1,26 @@
 import { db } from '../firebaseconfig'
 import { doc, deleteDoc } from 'firebase/firestore';
-import { List, ListItem, ListItemText } from "@material-ui/core"
+import { Avatar, List, ListItem, ListItemAvatar, ListItemText } from "@material-ui/core"
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever'
 import './Todo.css'
 import moment from 'moment';
+import { BorderClear } from '@material-ui/icons';
 
-const Todo = ({arr}) =>{
-    console.log(arr)
-    return(
+const Todo = ({ arr }) => {
+    return (
         <List className="todo__list">
             <ListItem>
-                <ListItemText primary={arr.item.todo} 
-                    secondary={arr.item.timestamp ? moment(arr.item.timestamp.second).format('DD/MM/YYYY HH:mm') : ''}/>
+                <ListItemAvatar>
+                    <Avatar>
+                        <BorderClear />
+                    </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={arr.item.todo}
+                    secondary={arr.item.timestamp ? moment(arr.item.timestamp.second).format('DD/MM/YYYY HH:mm') : ''} />
             </ListItem>
             <DeleteForeverIcon fontSize='large' onClick={() => {
-                deleteDoc(doc(db,'todos',arr.id))
-            }}/>
+                deleteDoc(doc(db, 'todos', arr.id))
+            }} />
         </List>
     )
 }
